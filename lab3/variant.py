4c324b00e7d1e48355318e1d238e372e13a8a649
Kirillub64 <kiril_shep.2008@mail.ru>
2026-09-17 01:49:03 +0300

    лаба3

---
+++ lab3/variant.py
@@ -0,0 +1,10 @@
+value = 50
+
+if value < 0 or value > 100:
+    print("Ошибка диапазона")
+elif value <= 44:
+    print("Есть место")
+elif value <= 94:
+    print("Архив растёт")
+else:
+    print("Почти заполнен")
